c47ae0cd656d20003210e4136fd17f90a31719e1
mayadi1 <admin@ayadios.com>
2017-02-25 11:05:39 -0800

    First Commit

---
+++ Immigrants/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Immigrants
+//
+//  Created by Mohamed Ayadi on 2/25/17.
+//  Copyright © 2017 Mohamed Ayadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
